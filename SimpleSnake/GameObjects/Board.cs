28651d0e4f67ddd26baf2e5e5a0180f7ada64598
Drexysw <knevdani@gmail.com>
2024-05-30 14:03:34 +0300

    Add project files.

---
+++ SimpleSnake/GameObjects/Board.cs
@@ -0,0 +1,35 @@
+﻿namespace SimpleSnake.GameObjects
+{
+    internal class Board
+    {
+        public Board(int rows, int cols)
+        {
+            InitializeBoard();
+        }
+
+        private void InitializeBoard()
+        {
+            DrawVerticals();
+            DrawHorizontals();
+        }
+
+
+        private void DrawVerticals()
+        {
+            for (int i = 0; i < UPPER; i++)
+            {
+                
+
+            }
+        }
+        private void DrawHorizontals() 
+        {
+            for (int i = 0; i < LEFT; i++)
+            {
+                
+
+            }
+        }
+
+    }
+}
---
+++ SimpleSnake/GameObjects/Point.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleSnake.GameObjects
+{
+    internal class Point
+    {
+        protected int CurrentRow { get; set; }
+        protected int CurrentCol { get; set; }
+    }
+}
